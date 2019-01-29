2ca291f62a4d01944e7c57c3c6a66fc05e81acf8
Andrew Ward <andrewfranciswardii@gmail.com>
2019-01-29 13:08:47 -0500

    uploading js practice

---
+++ practice.js
@@ -0,0 +1,27 @@
+//console.log(6+6);
+//console.log(6*6);
+//console.log(4-3);
+//console.log("Angel");
+//console.log('"Hellow", he said.');
+//let a=5
+//console.log(a);
+// console.log("hellow");
+// this is my first javascript
+console.log("Andrew");
+let name = "Andrew"
+console.log(name);
+console.log(6+6-3);
+console.log(name);
+
+let a = 3
+let b = 5
+let c = a + b
+console.log(c);
+
+let x = 4, y = 7;
+console.log(x+y);
+const WYNCODE = 'Wyncode';
+for (let counter = 1; counter <= 10; counter++)
+{
+  console.log('Andrew');
+}
